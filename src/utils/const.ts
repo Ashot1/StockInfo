3084d12854fe7ebb835285c8981fc7480e73ce51
TvoyOtec <44757072+TvoyOtec@users.noreply.github.com>
2023-12-20 20:37:39 +0300

    0.0.1.8

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,1 +1,10 @@
 export const SiteURL = "https://stock-info-theta.vercel.app";
+
+export const URLList = {
+    front: "/front",
+    home: "/home",
+    news: "/news",
+    stocks: "/stocks",
+    bonds: "/bonds",
+    currency: "/currency",
+};
