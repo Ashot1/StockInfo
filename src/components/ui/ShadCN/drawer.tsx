077ebcc63d8a58db2b80af8f6d5f78ff6793b56c
TvoyOtec <44757072+TvoyOtec@users.noreply.github.com>
2024-08-04 18:17:12 +0300

    Decrease modal backdrop blur

--- src/components/ui/ShadCN/drawer.tsx
+++ src/components/ui/ShadCN/drawer.tsx
@@ -45,7 +45,7 @@ const DrawerContent = React.forwardRef<
       <DrawerPrimitive.Content
          ref={ref}
          className={cn(
-            'fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto max-h-[90dvh] flex-col rounded-t-[10px] border outline-none backdrop-blur-3xl',
+            'fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto max-h-[90dvh] flex-col rounded-t-[10px] border outline-none backdrop-blur-2xl',
             className
          )}
          {...props}
