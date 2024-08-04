8253d93c3ea96b31326760c6cef6fa90ec6029b4
TvoyOtec <44757072+TvoyOtec@users.noreply.github.com>
2024-08-04 13:50:17 +0300

    Fix keyboard accessability

--- src/components/ui/CheckBox/CheckBox.tsx
+++ src/components/ui/CheckBox/CheckBox.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { ChangeEvent, FC, ReactNode } from 'react'
+import { ChangeEvent, FC, ReactNode, useRef } from 'react'
 import { cn } from '@/utils/utils'
 
 export type TCheckBox = {
@@ -11,11 +11,20 @@ export type TCheckBox = {
 }
 
 const CheckBox: FC<TCheckBox> = ({ className, checked, click, children }) => {
+   const ref = useRef<HTMLInputElement | null>(null)
    return (
       <label
-         className={cn('relative block h-6 w-12 cursor-pointer', className)}
+         className={cn(
+            'relative block h-6 w-12 cursor-pointer rounded-2xl',
+            className
+         )}
+         tabIndex={0}
+         onKeyUp={(e) => {
+            if (e.code === 'Enter' && ref.current) ref.current.click()
+         }}
       >
          <input
+            ref={ref}
             type="checkbox"
             className="peer hidden"
             checked={checked}
@@ -23,10 +32,7 @@ const CheckBox: FC<TCheckBox> = ({ className, checked, click, children }) => {
          />
          <div className="h-full w-full rounded-2xl bg-neutral-600 opacity-15 peer-checked:bg-green-600 dark:bg-[#aeaaae] dark:peer-checked:bg-[#84da89]" />
          <span
-            className={`pointer-events-none absolute 
-            left-[10%] top-[10%] h-[80%] w-[40%] 
-            rounded-full bg-[var(--Main)] opacity-70 duration-300 
-            peer-checked:left-[50%] peer-checked:bg-green-600`}
+            className={`pointer-events-none absolute left-[10%] top-[10%] h-[80%] w-[40%] rounded-full bg-[var(--Main)] opacity-70 duration-300 peer-checked:left-[50%] peer-checked:bg-green-600`}
          />
          {children}
       </label>
