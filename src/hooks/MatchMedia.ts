88056afbdb7683a2f6093b42bd22f94bb059b684
TvoyOtec <44757072+TvoyOtec@users.noreply.github.com>
2024-08-04 16:16:40 +0300

    Improve matchMedia logic

--- src/hooks/MatchMedia.ts
+++ src/hooks/MatchMedia.ts
@@ -4,22 +4,22 @@ import { useEffect, useState } from 'react'
 
 export type TUseMatchMedia = (maxWidth: number | string) => boolean | null
 
-export const useMatchMedia: TUseMatchMedia = (maxWidth) => {
+export const useMatchMedia: TUseMatchMedia = (query) => {
    const [MatchState, setMatchState] = useState<boolean | null>(null)
 
    useEffect(() => {
       const checkSize = () => {
          const condition = window.matchMedia(
-            typeof maxWidth === 'number'
-               ? `(max-width: ${maxWidth}px)`
-               : maxWidth
+            typeof query === 'number' ? `(max-width: ${query}px)` : query
          ).matches
 
          setMatchState(condition)
       }
       checkSize()
       window.addEventListener('resize', checkSize)
-   }, [maxWidth])
+
+      return () => window.removeEventListener('resize', checkSize)
+   }, [query])
 
    return MatchState
 }
