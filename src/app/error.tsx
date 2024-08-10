66128a50ae5c387fcebce9b3cb76bf81de5950d6
TvoyOtec <44757072+TvoyOtec@users.noreply.github.com>
2024-08-10 16:26:29 +0300

    Improve error logs on error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -11,7 +11,7 @@ export default function Error({
    error: Error & { digest?: string }
    reset: () => void
 }) {
-   useEffect(() => console.log(error), [error])
+   useEffect(() => console.error(error), [error])
 
    return (
       <AuroraBackground allowFullScreen={true}>
